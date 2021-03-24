f4d214cb883691c74215b8e41b0ddaf0d54933ac
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-03-24 16:42:48 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -305,18 +305,33 @@ router.get('/tiktod', async (req, res, next) => {
 	if(apikeyInput != 'FreeApi') return res.json(loghandler.invalidKey)
      if (!url) return res.json(loghandler.noturl)
 
-     TikTokScraper.getVideoMeta(url, options)
-         .then(vid => {
-             console.log(vid)
-             res.json({
-                 status: true,
-                 creator: `${creator}`,
-                 videoNoWm: vid
-             })
-         })
-         .catch(e => {
-             res.json(loghandler.invalidlink)
-         })
+     const puppeteer = require("puppeteer");
+
+async function getVideo(URL) {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.goto('https://snaptik.app/');
+
+    await page.type('#url', `${URL}`);
+    await page.click('#send', { delay: 300 });
+
+    await page.waitForSelector('#download-block > div > a:nth-child(3)', {delay: 300});
+    let mp4direct = await page.$eval("#download-block > div > a:nth-child(3)", (element) => {
+        return element.getAttribute("href");
+    });
+    let image = await page.$eval("#div_download > section > div > div > div > article > div.zhay-left.left > img", (element) => {
+        return element.getAttribute("src");
+    });
+	let textInfo = await page.$eval('#div_download > section > div > div > div > article > div.zhay-middle.center > p:nth-child(2) > span', el => el.innerText);
+	let nameInfo = await page.$eval('#div_download > section > div > div > div > article > div.zhay-middle.center > h1 > a', el => el.innerText);
+	let timeInfo = await page.$eval('#div_download > section > div > div > div > article > div.zhay-middle.center > p:nth-child(3) > b', el => el.innerText);
+	browser.close();
+    return { mp4direct, image, textInfo, nameInfo, timeInfo }
+}
+ res.json(await getVideo(url))
 })
 
 router.get('/tiktod/stalk', async (req, res, next) => {
