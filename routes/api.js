865f8b6b98cd05a13b7030cb7be665aa9eeb1788
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-03-26 14:47:02 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -645,28 +645,9 @@ router.get('/nulis', async (req, res, next) => {
 	if(!apikeyInput) return res.json(loghandler.notparam)
      if(apikeyInput != 'FreeApi') return res.sendFile(__path + '/routes/invalidkey.html')
 	 if(!text) return res.json(loghandler.nottext)
-		Nulis(text)
-		 .then(hasil => {
-			fetch(encodeURI(`https://api.imgbb.com/1/upload?expiration=120&key=761ea2d5575581057a799d14e9c78e28&image=${hasil}&name=${randomTextNumber}`))
-                                .then(response => response.json())
-                                .then(data => {
-                                    var urlnya = data.data.url,
-                                        delete_url = data.data.delete_url;
-                                        res.json({
-                                            status : true,
-                                            creator : `${creator}`,
-                                            message : `jangan lupa follow ${creator}`,
-                                            result:{
-                                                url:urlnya,
-                                                delete_url: delete_url,
-                                                info: 'url akan hilang setelah 2 menit'
-                                            }
-                                        })
-                                })
-            })
-           .catch(err => {
-		  res.json(loghandler.error)
-		   })
+         rest = await fetch(`http://salism3.pythonanywhere.com/write?text=${text}`)
+         restt = await rest.json()
+ 	res.json({ status: true, url: restt.images[0] })
 })
 
 router.get('/textmaker', async (req, res, next) => {
